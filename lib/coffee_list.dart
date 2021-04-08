75d520a53db04936177c0af95e6a657269217869
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-07 19:06:13 -0600

    page view 3d perspective scroll

--- lib/coffee_list.dart
+++ lib/coffee_list.dart
@@ -1,3 +1,4 @@
+import 'package:coffe_app/coffee.dart';
 import 'package:flutter/material.dart';
 
 class CoffeeList extends StatefulWidget {
@@ -6,8 +7,96 @@ class CoffeeList extends StatefulWidget {
 }
 
 class _CoffeeListState extends State<CoffeeList> {
+  final _pageCoffeeController = PageController(
+    viewportFraction: .35,
+  );
+  double _currentPage = 0;
+
+  void _coffeeScrollListener() {
+    setState(() {
+      _currentPage = _pageCoffeeController.page;
+    });
+  }
+
+  @override
+  void initState() {
+    _pageCoffeeController.addListener((_coffeeScrollListener));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageCoffeeController.removeListener((_coffeeScrollListener));
+    _pageCoffeeController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 20,
+            right: 20,
+            bottom: -size.height * 0.22,
+            height: size.height * 0.3,
+            child: DecoratedBox(
+                decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+              BoxShadow(color: Colors.brown, blurRadius: 90, spreadRadius: 45)
+            ])),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 100,
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Transform.scale(
+            scale: 1.6,
+            alignment: Alignment.bottomCenter,
+            child: PageView.builder(
+                controller: _pageCoffeeController,
+                scrollDirection: Axis.vertical,
+                itemCount: coffees.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return const SizedBox.shrink();
+                  }
+                  final coffee = coffees[index - 1];
+                  final result = _currentPage - index + 1;
+                  final value = -0.4 * result + 1;
+                  final opacity = value.clamp(0.0, 1.0);
+
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Transform(
+                        alignment: Alignment.bottomCenter,
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001)
+                          ..translate(
+                              0.0, size.height / 2.6 * (1 - value).abs())
+                          ..scale(value),
+                        child: Opacity(
+                            opacity: opacity,
+                            child: Image.asset(
+                              coffee.image,
+                              fit: BoxFit.fitHeight,
+                            ))),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
   }
 }
